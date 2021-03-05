11fa3f60fbf8e6ffba3dbafdd518f2408ad91422
Greatmedivack <putinihin@medods.ru>
2021-03-05 12:08:51 +0500

    generate template commit

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,8 +1,25 @@
 class Redirect < ApplicationRecord
-	before_create :create_template
 
-	def create_template
-		puts "template creating"
+	CONFIGS_PATH = "nginx/configs/"
+	TEMPLATE_PATH = "nginx/template.conf"
+
+	before_create :create_nginx_config_file
+
+	def create_nginx_config_file
+
+		template_file = File.open(TEMPLATE_PATH).read
+		config_file = File.new(formated_config_file_name,"w")
+		template_file.each_line do |line|
+  			line.gsub!(/\%SERVER_NAME\%/, self.domain)
+  			line.gsub!(/\%URL\%/, self.url)
+  			config_file.puts(line)
+		end
+		config_file.close
 	end
+
+	def formated_config_file_name
+		CONFIGS_PATH + self.domain.downcase.gsub(/\./,'_') + '.conf'
+	end
+
 
 end
